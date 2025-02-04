51b8e747fde43544189d22dc11b290dc776a81ea
KaiqueFj <kaiquelferraz@hotmail.com>
2025-02-03 22:10:49 -0300

    added optimization on atomic-blog using (useMemo & useCallback)

--- atomic-blog/src/PostContext.js
+++ atomic-blog/src/PostContext.js
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 
 // 1 - Create new context
 const PostContext = createContext();
@@ -35,18 +35,18 @@ function PostProvider({ children }) {
     setPosts([]);
   }
 
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchedPosts, searchQuery]);
+
   return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      {children}
-    </PostContext.Provider>
+    <PostContext.Provider value={{ value }}>{children}</PostContext.Provider>
   );
 }
 
