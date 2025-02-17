62921089c6673556d7bcd78d858f05fae8726664
KaiqueFj <kaiquelferraz@hotmail.com>
2025-02-17 19:54:15 -0300

    feat: added the features as cleaning the cart, adding the item and deleating it using the rdk

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled, to, type }) {
+export default function Button({ children, disabled, to, type, onClick }) {
   const base =
     "inline-block text-sm  font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full  disabled:cursor-not-allowed focus:ring-offset-2 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:outline-none hover:bg-yellow-300 text-stone-800";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs ",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm ",
     secondary:
       "inline-block   text-sm font-semibold border-2 border-stone-300 tracking-wide uppercase transition-colors duration-300 bg-transparent rounded-full  disabled:cursor-not-allowed focus:ring-offset-2 focus:bg-yellow-300 focus:ring focus:ring-stone-200 focus:outline-none hover:bg-stone-300 hover:text-stone-800  focus:text-stone-800 text-stone-400 px-4 py-2.5 md:px-6 md:py-3.5",
   };
@@ -16,6 +17,13 @@ export default function Button({ children, disabled, to, type }) {
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
+    );
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
     );
 
   return (
