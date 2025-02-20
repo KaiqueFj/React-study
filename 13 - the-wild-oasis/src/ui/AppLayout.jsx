113fde87b37a1dd408adf4720e21fa477bce35c3
KaiqueFj <kaiquelferraz@hotmail.com>
2025-02-19 21:24:41 -0300

    feat: Added the project structure and layout (header, sidebar, children being rendered)

--- 13 - the-wild-oasis/src/ui/AppLayout.jsx
+++ 13 - the-wild-oasis/src/ui/AppLayout.jsx
@@ -9,7 +9,6 @@ const StyledAppLayout = styled.div`
   grid-template-rows: auto 1fr;
   height: 100vh;
 `;
-
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
