b5e41ba99aa1005f64141b7ba0913cacc43ecfdf
KaiqueFj <kaiquelferraz@hotmail.com>
2025-03-20 00:54:48 -0300

    feat: added the feature to update user data and password

--- 13 - the-wild-oasis/src/services/apiAuth.js
+++ 13 - the-wild-oasis/src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signUp({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -44,4 +44,39 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
 
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1 - update password or fullName
+
+  let updateData;
+
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2 - Upload the avatar image
+  const filename = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(filename, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3 - update avatar in the user
+  const { data: updatedUser, error: errorUpdatedUser } =
+    supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${filename}`,
+      },
+    });
+
+  if (errorUpdatedUser) throw new Error(errorUpdatedUser.message);
+
+  return updatedUser;
 }
