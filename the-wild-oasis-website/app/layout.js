3a860f625815336bd82373b069ca0af41faa2837
KaiqueFj <kaiquelferraz@hotmail.com>
2025-03-26 21:47:20 -0300

    style: adding styles

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -2,6 +2,7 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import { Josefin_Sans } from "next/font/google";
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 const josefinFont = Josefin_Sans({
   subsets: ["latin"],
@@ -21,14 +22,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={` ${josefinFont.className} min-h-screen bg-primary-950 text-primary-100`}
+        className={` ${josefinFont.className} min-h-screen antialiased bg-primary-950 flex flex-col text-primary-100`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl ">{children}</main>
+        </div>
       </body>
     </html>
   );
